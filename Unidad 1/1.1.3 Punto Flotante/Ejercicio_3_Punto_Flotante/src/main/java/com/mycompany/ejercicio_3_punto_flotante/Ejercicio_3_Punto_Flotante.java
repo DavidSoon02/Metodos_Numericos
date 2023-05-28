5aeb93abaecd63ad6d93ed8fa9cfaf0987710f8e
EddyFight39 <87662601+EddyFight39@users.noreply.github.com>
2023-05-28 13:02:53 -0500

    Ejercicios grupales

---
+++ Unidad 1/1.1.3 Punto Flotante/Ejercicio_3_Punto_Flotante/src/main/java/com/mycompany/ejercicio_3_punto_flotante/Ejercicio_3_Punto_Flotante.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ejercicio_3_punto_flotante;
+
+/**
+ *
+ * @author Daniel A
+ */
+public class Ejercicio_3_Punto_Flotante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
