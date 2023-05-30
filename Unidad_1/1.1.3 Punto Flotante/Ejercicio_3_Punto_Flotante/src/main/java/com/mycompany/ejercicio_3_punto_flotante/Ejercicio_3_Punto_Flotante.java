27b94d607704fd74cab0e5db335aab240dd0fe6c
AngeloDUF <134798365+AngeloDUF@users.noreply.github.com>
2023-05-29 20:14:22 -0500

    Update Ejercicio_3_Punto_Flotante.java

--- Unidad_1/1.1.3 Punto Flotante/Ejercicio_3_Punto_Flotante/src/main/java/com/mycompany/ejercicio_3_punto_flotante/Ejercicio_3_Punto_Flotante.java
+++ Unidad_1/1.1.3 Punto Flotante/Ejercicio_3_Punto_Flotante/src/main/java/com/mycompany/ejercicio_3_punto_flotante/Ejercicio_3_Punto_Flotante.java
@@ -10,8 +10,19 @@ package com.mycompany.ejercicio_3_punto_flotante;
  * @author Daniel A
  */
 public class Ejercicio_3_Punto_Flotante {
+public static void main(String[] args) {
+        int a = 150; // Valor máximo para un entero
+        int b = 260; // Valor a sumar
+        
+        int sum = a + b; // Realizar la suma
+        
+        // Verificar si se produce un desbordamiento
+        if ((a > 0 && b > 0 && sum < 0) || (a < 0 && b < 0 && sum >= 0)) {
+            System.out.println("Desbordamiento detectado.");
+        } else {
+            System.out.println("No se produjo desbordamiento.");
+        }
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    
 }
